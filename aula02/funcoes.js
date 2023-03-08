52f08bfc64e1637853af21996ddac2f1a722a22f
dudamartine <eduardamariafm15@gmail.com>
2023-03-08 08:42:07 -0300

    Aula sobre javascript com funcoes e variaveis

---
+++ aula02/funcoes.js
@@ -0,0 +1,19 @@
+let num1 = 1
+const num2 = 3
+function soma() { 
+    return num1 + num2 
+}
+
+console.log(soma())
+
+num1 = 6
+// num2 = 9
+
+console.log(soma())
+
+const funcaoSoma = (a, b) => {
+    return a + b
+}
+console.log (funcaoSoma(10, 8))
+console.log (funcaoSoma(30, 8))
+
---
+++ aula02/funcoesevariaveis.js
@@ -0,0 +1,14 @@
+var variavel1 = "ola"
+let variavel2 = 4
+const variavel3 = 8
+const obj = {
+    nome: "Duda",
+    signo: "Leao"
+   
+}
+
+console.log(variavel1, "Turma do", variavel2)
+console.log(variavel2 + variavel3)
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+
