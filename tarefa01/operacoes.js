4d0bf54ac5ebefcef7817e133ccf2aed69d41f3a
dudamartine <eduardamariafm15@gmail.com>
2023-03-15 07:30:41 -0300

    Fiz as tarefas de operadores

---
+++ tarefa01/operacoes.js
@@ -0,0 +1,30 @@
+let a = 1
+let b = 3
+function soma() { 
+    return a + b
+}
+function sub (){
+    return a - b	
+}
+function mult(){
+    return a*b
+}
+function div(){
+return a/b
+}
+function exp(){
+    return a**b
+}
+function rad(){
+    return Math.sqrt(a)
+}
+
+
+console.log(soma(a,b))
+console.log(sub(a,b))
+console.log(mult(a,b))
+console.log(div(a,b))
+console.log(exp(a,b))
+console.log(rad(a))
+
+
